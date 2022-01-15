5a00b4d02eee345cd46a73aead745d99f3660c64
GunayM <gunay.nur1303@gmail.com>
2022-01-15 10:20:10 -0500

    Palindrome task

---
+++ src/Palindrome.java
@@ -0,0 +1,13 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String ward= new String("Level");
+        String polindromeWord="";
+
+        for (int i = ward.length()-1; i >=0; i--) {
+            polindromeWord+=ward.charAt(i);
+
+        }
+        System.out.println(polindromeWord.equalsIgnoreCase(ward));
+
+    }
+}
