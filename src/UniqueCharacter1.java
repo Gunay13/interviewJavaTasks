7d56d75a88c0dc79974419af6b7e51fdb92b8dcf
GunayM <gunay.nur1303@gmail.com>
2022-01-15 10:33:11 -0500

    Find Unique character task 1

---
+++ src/UniqueCharacter1.java
@@ -0,0 +1,16 @@
+public class UniqueCharacter1 {
+    public static void main(String[] args) {
+
+        String str="ABBCCDEEFG";
+        String unique="";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i))){
+                unique+=str.charAt(i);
+            }
+        }
+        System.out.println(unique);
+
+
+    }
+}
