7ec09891dbeb6b2c6cf6d6749367cca043011dca
GunayM <gunay.nur1303@gmail.com>
2022-01-15 11:49:25 -0500

    Sort ascending array

---
+++ src/arrayTasks/SortAscending.java
@@ -0,0 +1,13 @@
+package arrayTasks;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class SortAscending {
+    public static void main(String[] args) {
+        int[] arr={5,4,1,8,7,2,3,4,9,6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
