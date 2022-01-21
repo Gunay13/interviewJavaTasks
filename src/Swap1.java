e53d786d9e638722727b85986b9ac5aa86deb698
GunayM <gunay.nur1303@gmail.com>
2022-01-20 23:58:40 -0500

    Swap task 1

---
+++ src/Swap1.java
@@ -0,0 +1,17 @@
+public class Swap1 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("*******************");
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+    }
+}
